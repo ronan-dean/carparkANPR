03d7d88a073dae51f527ce1339f29868c902e7ff
dgb224 <ultraboyz63@gmail.com>
2021-03-12 12:04:06 +1100

    first commit

---
+++ testing.py
@@ -0,0 +1,30 @@
+import pymongo
+from datetime import datetime
+from datetime import timedelta
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+mydb = myclient["carpark"]
+
+mycol = mydb["testing"]
+action = input("1. Find Time 2. Enter car 3. Exit car")
+if action == "1":
+    find = input("Enter car plate: ")
+    query = { "plate": find }
+    result = mycol.find(query,{"_id": 0 })
+    for x in result:
+        print(x)
+if action == "2":
+    enter = input("Car plate: ")
+    user = {"plate": enter, "time": datetime.now(), "exited": False}
+    mycol.insert_one(user)
+    print("car entered")
+if action == "3":
+    exit = input("Car plate to exit: ")
+    query = {"plate": exit}
+    mycol.update_many(query, {"$set": { "exited": True}})
+    print("car exited")
+
+
+
+##user = { "_id": "1", "plate": "1234", "time": "12"}
+##x = mycol.insert_one(user)

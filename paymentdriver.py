bfc8a52a2c1684e0db02f21d32a68e040a13bd1c
Ronan Dean <ultraboyz63@gmail.com>
2021-05-11 22:49:12 +1000

    Updated payment system

---
+++ paymentdriver.py
@@ -0,0 +1,45 @@
+import pymongo
+from datetime import datetime
+from datetime import timedelta
+# car park pymongoset up
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["carpark"]
+mycol = mydb["paymenttesting"]
+# card payment pymongo set up 
+paydb = myclient["carpark"]
+paycol = paydb["cardtesting"]
+
+mycard = "1234"
+myplate = "abc-def"
+query = { "card": mycard }
+cursor = mycol.find(query)
+
+count = mycol.count_documents({"plate": myplate})
+if count == 1:
+    print("This is a new car")
+if count == 0:
+    print("Bad reading from plate")
+if count > 1:
+    print("searching for non exited stamp")
+    carCount = mycol.count_documents({"plate": myplate, "exited": False})
+    if carCount == 1:
+        print("Found an unexited car")
+        carResult = mycol.find_one({"plate": myplate, "exited": False})
+        print(carResult["time"])
+        card = carResult["card"]
+        payCard = paycol.find_one({"card": card})
+        print(payCard)
+        timeInPark = datetime.now() - carResult["time"]
+        print("This car spent", timeInPark, "time in the car park")
+        mins = timeInPark.total_seconds() / 60
+        print("Total time in mins", mins)
+        if mins <= 60:
+            print("cost is $2")
+        if mins > 60 and mins < 119:
+            print("cost is $4")
+        if mins > 120 and mins < 179:
+            print("cost is $6")
+        
+        
+
+    
